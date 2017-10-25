c2dcfcd0da9cc20c6c8a2b22518a81c3944353ea
Zeyu Ni <zeyuni827@gmail.com>
2017-10-25 00:38:32 -0400

    order service

---
+++ spring-cloud-food-delivery/order-service/src/main/java/demo/OrderServiceApplication.java
@@ -0,0 +1,8 @@
+package demo;
+
+@SpringBootApplication
+public class OrderServiceApplication {
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(OrderServiceApplication.class, args);
+    }
+}
